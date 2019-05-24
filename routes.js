17ad4624b4a83121d7fc4b4a2db87bd484e089e6
Nguyen Anh <cauut2117610@gmail.com>
2019-05-24 09:26:28 +0700

    add schedules

--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
 exports.routes = {
-	"root" : "/",
-	"save" : "/save",
-	"run" : "/runjob",
-	"crontab" : "/crontab",
-	"stop" : "/stop",
-	"start" : "/start",
-	"remove": "/remove",
-	"backup": "/backup",
-	"restore": "/restore",
-	"delete_backup": "/delete",
-	"restore_backup": "/restore_backup",
-	"export": "/export",
-	"import": "/import", // this is import from database
-	"import_crontab": "/import_crontab", // this is from existing crontab
-	"logger": "/logger",
+	"root" : "/schedules",
+	"save" : "/schedules/save",
+	"run" : "/schedules/runjob",
+	"crontab" : "/schedules/crontab",
+	"stop" : "/schedules/stop",
+	"start" : "/schedules/start",
+	"remove": "/schedules/remove",
+	"backup": "/schedules/backup",
+	"restore": "/schedules/restore",
+	"delete_backup": "/schedules/delete",
+	"restore_backup": "/schedules/restore_backup",
+	"export": "/schedules/export",
+	"import": "/schedules/import", // this is import from database
+	"import_crontab": "/schedules/import_crontab", // this is from existing crontab
+	"logger": "/schedules/logger",
 };
